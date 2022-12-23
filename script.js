1d98f0017a9d6898ec4dfc83fbc4629d4ca997cc
rafaelafpro <66646053+rafaelafpro@users.noreply.github.com>
2022-12-22 21:34:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+const form = document.getElementById("form");
+const output = document.getElementById("output");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const url = document.getElementById("url").value;
+
+    scrape(url).then((data) => {
+        output.innerHTML = "";
+        const table = createTable(data);
+        output.appendChild(table);
+    });
+});
+
+async function scrape(url) {
+    const res = await fetch(url);
+    const html = await res.text();
+
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, "text/html");
+
+    const table = doc.querySelector("table");
+    const rows = table.querySelectorAll("tr");
+    const data = [];
+
+    for (const row of rows) {
+        const cols = row.querySelectorAll("td");
+        const cells = [];
+        for (const col of cols) {
+            cells.push(col.textContent);
+        }
+        data.push(cells);
+    }
+
+    return data;
+}
+
+function createTable(data) {
+    const table = document.createElement("table");
+
+    for (const row of data) {
+        const tr = document.createElement("tr");
+        for (const cell of row) {
+            const td = document.createElement("td");
+            td.textContent = cell;
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
+
+    return table;
+}
