dbbfededf96a0be5039b2b94cb2f2518d9913c60
rafaelafpro <66646053+rafaelafpro@users.noreply.github.com>
2022-12-22 21:44:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,48 @@
-const form = document.getElementById("form");
-const output = document.getElementById("output");
+const form = document.getElementById('form');
+const output = document.getElementById('output');
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-    const url = document.getElementById("url").value;
+  const url = document.getElementById('url').value;
 
-    scrape(url).then((data) => {
-        output.innerHTML = "";
-        const table = createTable(data);
-        output.appendChild(table);
+  fetch(url)
+    .then((res) => res.text())
+    .then((html) => {
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(html, 'text/html');
+
+      const table = doc.querySelector('table');
+      const rows = table.querySelectorAll('tr');
+      const data = [];
+
+      for (const row of rows) {
+        const cols = row.querySelectorAll('td');
+        const cells = [];
+        for (const col of cols) {
+          cells.push(col.textContent);
+        }
+        data.push(cells);
+      }
+
+      output.innerHTML = '';
+      const processedTable = createTable(data);
+      output.appendChild(processedTable);
     });
 });
 
-async function scrape(url) {
-    const res = await fetch(url);
-    const html = await res.text();
-
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(html, "text/html");
-
-    const table = doc.querySelector("table");
-    const rows = table.querySelectorAll("tr");
-    const data = [];
-
-    for (const row of rows) {
-        const cols = row.querySelectorAll("td");
-        const cells = [];
-        for (const col of cols) {
-            cells.push(col.textContent);
-        }
-        data.push(cells);
-    }
-
-    return data;
-}
-
 function createTable(data) {
-    const table = document.createElement("table");
+  const table = document.createElement('table');
 
-    for (const row of data) {
-        const tr = document.createElement("tr");
-        for (const cell of row) {
-            const td = document.createElement("td");
-            td.textContent = cell;
-            tr.appendChild(td);
-        }
-        table.appendChild(tr);
+  for (const row of data) {
+    const tr = document.createElement('tr');
+    for (const cell of row) {
+      const td = document.createElement('td');
+      td.textContent = cell;
+      tr.appendChild(td);
     }
+    table.appendChild(tr);
+  }
 
-    return table;
+  return table;
 }
